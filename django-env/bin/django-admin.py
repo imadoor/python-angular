a6a9fffeea8423865122e4762e1a295644bbb957
Amador Ollarzabal <amadorollarzabal@Amadors-Mini.attlocal.net>
2020-03-03 20:29:23 -0600

    initial commit for the django rest/crud app

---
+++ django-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/amadorollarzabal/dev/python-project/django-project-1/django-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
